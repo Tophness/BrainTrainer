8550f09ee9e7757630f642288bd0d516eb9014d1
Tophness <chrismrulz@hotmail.com>
2014-11-20 00:23:01 +1100

    BrainTrainer
    Subconscious Trainer Thingy I Gone And Done

---
+++ BrainTrainer/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.bottomimg = New System.Windows.Forms.PictureBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.topimg = New System.Windows.Forms.PictureBox
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.bottomimg, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.topimg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bottomimg
+        '
+        Me.bottomimg.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.bottomimg.Location = New System.Drawing.Point(0, 0)
+        Me.bottomimg.Name = "bottomimg"
+        Me.bottomimg.Size = New System.Drawing.Size(504, 462)
+        Me.bottomimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.bottomimg.TabIndex = 0
+        Me.bottomimg.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(208, 212)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Begin"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'topimg
+        '
+        Me.topimg.Location = New System.Drawing.Point(147, 282)
+        Me.topimg.Name = "topimg"
+        Me.topimg.Size = New System.Drawing.Size(196, 168)
+        Me.topimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.topimg.TabIndex = 3
+        Me.topimg.TabStop = False
+        Me.topimg.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Timer2
+        '
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Showcard Gothic", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(480, 110)
+        Me.Label1.TabIndex = 4
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label1.Visible = False
+        '
+        'Timer3
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(504, 462)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.topimg)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.bottomimg)
+        Me.Name = "Form1"
+        Me.Text = "Subconscious Trainer"
+        CType(Me.bottomimg, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.topimg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents bottomimg As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents topimg As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+
+End Class
---
+++ BrainTrainer/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+    Dim stagesperround As Integer = 10
+
+    Dim round As Integer = 1
+    Dim direction As String = "left"
+    Dim count As Integer = 0
+    Dim pressed As String = ""
+    Dim score As Integer = 0
+    Dim maxscore As Integer = 0
+    Dim score2 As Integer = 0
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        round = 1
+        direction = "left"
+        count = 0
+        pressed = ""
+        score = 0
+        maxscore = 0
+        score2 = 0
+
+        Label1.Visible = False
+        Button1.Hide()
+        Start()
+    End Sub
+
+    Public Sub Start()
+        Dim lr As Integer = GetRandom(0, 100)
+        If (lr < 50) Then
+            direction = "left"
+        Else
+            direction = "right"
+        End If
+        bottomimg.ImageLocation = Application.StartupPath & "\round" & round & "_" & direction & ".gif"
+        pressed = ""
+    End Sub
+
+    Public Function check()
+        If (round < 10) Then
+            If (count <= stagesperround) Then
+                Dim longround As Integer = 6282 / round
+                Dim lr As Integer = GetRandom(0, 100)
+                If (lr < 50) Then
+                    direction = "left"
+                Else
+                    direction = "right"
+                End If
+                count += 1
+                bottomimg.ImageLocation = Application.StartupPath & "\round" & round & "_" & direction & ".gif"
+                pressed = "default"
+                Timer1.Interval = GetRandom(Int(longround / 2), longround)
+                Timer1.Start()
+            Else
+                maxscore += stagesperround
+                Label1.Visible = True
+                Label1.Text = "Score: " & score2 & " ( " & Int((score / maxscore) * 100) & "%)"
+                Timer3.Interval = 3145
+                Timer3.Start()
+                Timer1.Stop()
+                round += 1
+                direction = "left"
+                count = 0
+                pressed = ""
+                Start()
+            End If
+        Else
+            Timer1.Stop()
+            Timer2.Stop()
+            Timer3.Stop()
+            topimg.Visible = False
+            bottomimg.ImageLocation = Application.StartupPath & "\finished.jpg"
+            Label1.Visible = True
+            Label1.Text = "Final Score: " & score2 & " ( " & Int((score / maxscore) * 100) & "%)"
+            Button1.Show()
+        End If
+    End Function
+
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        Dim Generator As System.Random = New System.Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As System.Windows.Forms.Message, ByVal keyData As System.Windows.Forms.Keys) As Boolean
+        If msg.WParam.ToInt32() = CInt(Keys.Left) Then
+            If (direction = "left") Then
+                pressed = "correct"
+            Else
+                pressed = "incorrect"
+            End If
+        ElseIf msg.WParam.ToInt32() = CInt(Keys.Right) Then
+            If (direction = "right") Then
+                pressed = "correct"
+            Else
+                pressed = "incorrect"
+            End If
+        End If
+        Dim longround As Integer = 6282 / round
+        If (pressed <> "") Then
+            Timer1.Stop()
+            Timer2.Stop()
+            Timer3.Stop()
+            topimg.Visible = True
+            topimg.ImageLocation = Application.StartupPath & "\" & pressed & ".png"
+            Timer2.Interval = longround / 2
+            Timer2.Start()
+            If (pressed = "correct") Then
+                score += 1
+                score2 += (1 * (round * 100))
+            End If
+            check()
+        End If
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        check()
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Timer2.Stop()
+        topimg.Visible = False
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer3.Tick
+        Timer3.Stop()
+        Label1.Visible = False
+    End Sub
+End Class
